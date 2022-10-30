b3d0d6a902bd9c7fd473e9846a22c84e4862bd8b
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-10-30 10:57:05 +0530

    Update carbon.py

--- plugins/modules/carbon.py
+++ plugins/modules/carbon.py
@@ -8,7 +8,7 @@ aiohttpsession = ClientSession()
 C = "Successfully Created Your Carbon"
 F = InlineKeyboardMarkup(
 [[
-     InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/dk_botx")
+     InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/botsupportastra")
 ]]
 )
 
