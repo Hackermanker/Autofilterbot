68aa7c01c5b199943cf14a917bd2e8ee7791803f
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-11-23 23:46:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
 from utils import temp
 
 class Bot(Client):
